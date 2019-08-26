b8fc374ffb46de80bb683f3045e1a9cdd402376a
rosalesnikho <rosales.nikho@gmail.com>
2019-08-26 15:44:47 -0500

    add projec logic

--- src/main/java/io/d3connect/d3connect/controller/ProjectController.java
+++ src/main/java/io/d3connect/d3connect/controller/ProjectController.java
@@ -1,9 +1,19 @@
 package io.d3connect.d3connect.controller;
 
+import io.d3connect.d3connect.domain.Project;
+import io.d3connect.d3connect.service.MapVaidator.MapValidationErrorService;
 import io.d3connect.d3connect.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -11,4 +21,21 @@ public class ProjectController {
 
     @Autowired
     ProjectService projectService;
+
+    @Autowired
+    MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/new")
+    public ResponseEntity<?> createNewProject (@Valid @RequestBody Project project, BindingResult result) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrorService(result);
+
+        // Return errors
+        if(errorMap != null) {
+            return errorMap;
+        }
+
+        //Return successful project creation and an HTTP Status of 2XX
+        Project projectSave = projectService.createOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
 }
--- src/main/java/io/d3connect/d3connect/service/ProjectService.java
+++ src/main/java/io/d3connect/d3connect/service/ProjectService.java
@@ -1,7 +1,22 @@
 package io.d3connect.d3connect.service;
 
+import io.d3connect.d3connect.domain.Project;
+import io.d3connect.d3connect.exception.ProjectException.ProjectIdException;
+import io.d3connect.d3connect.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectService {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    public Project createOrUpdateProject (Project project) {
+        try {
+            return projectRepository.save(project);
+        }catch (Exception e) {
+            throw  new ProjectIdException("ERROR Creating a project");
+        }
+    }
 }
